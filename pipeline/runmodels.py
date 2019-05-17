added chained unit test runmodels->fov, fixed normals latents having wrong shape

pipeline/runmodels.py
@@ -38,5 +38,6 @@ class PipelineRunModels(PipelineStep):
         latent_tensors = self.model_normals.graph.get_tensor_by_name(
             "generator/decoder_8/conv2d_transpose/BiasAdd:0")
         output_tensor = list(self.model_normals.feed_tensors.values())[0]
-        data["normals_latents"], data["normals"] = self.model_normals.session.run([latent_tensors, output_tensor],feed_dict={
+        normals_latents, data["normals"] = self.model_normals.session.run([latent_tensors, output_tensor],feed_dict={
             input_tensor: [cv2.resize(data["image"], (512, 512)).astype(np.float32)/255]})
+        data["normals_latents"] = normals_latents[:, :, :, :128].reshape(1, -1)