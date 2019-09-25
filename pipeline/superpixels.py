994219f358a918614d46509b06733868c6ef0b5f
Robin Kahlow <robinkahlow@web.de>
2019-09-25 15:51:12 +0100

    copied previous solution for superpixels and encoding them as an RGBA image

--- pipeline/superpixels.py
+++ pipeline/superpixels.py
@@ -15,6 +15,19 @@ class PipelineSuperpixels(PipelineStep):
 
     def run(self, data):
         image = data["image"]
-        superpixels = cv2.ximgproc.createSuperpixelSLIC(image)
-        superpixels.iterate(10)
-        data["superpixels"] = superpixels.getLabels()
+
+        superpixels = cv2.ximgproc.createSuperpixelLSC(image)
+        superpixels.iterate()
+        superpixels.enforceLabelConnectivity()
+
+        superpixel_image = superpixels.getLabels()
+        
+        # Convert 32 bit to 4x 8bit (https://stackoverflow.com/a/25298780)
+        bytes_dtype = np.dtype(("i4", [("bytes", "u1", 4)]))
+        superpixel_image = superpixel_image.view(dtype=bytes_dtype)["bytes"]
+
+        # Invert alpha channel. This serves no purpose other than visualization
+        # as otherwise all pixels would be transparent.
+        superpixel_image[:, :, 3] = 255 - superpixel_image[:, :, 3]
+
+        data["superpixels"] = superpixel_image
