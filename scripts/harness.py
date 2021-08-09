6c52646832a5b992002805397488e89550c7fabb
Joel Teply <joel@cambriantech.com>
2021-08-09 14:38:42 -0500

    harness for the pipeline refine step

---
+++ scripts/harness.py
@@ -0,0 +1,59 @@
+import math
+import os
+import json
+import numpy as np
+from pathlib import Path
+import pickle
+
+import click
+import time
+
+def get_file_paths(input_dir, pattern="*.pickle"):
+    files = []
+    files.extend(Path(input_dir).glob('**/' + pattern))
+            
+    return files
+
+def parse_data(input_dir, output_dir):
+    files = get_file_paths(input_dir, "*.pickle")
+
+    print("Importing %d files from \"%s\" into \"%s\"" % (len(files), input_dir, output_dir))
+
+    for path in files:
+        data_path = str(path)
+        dir_path = os.path.dirname(data_path)
+
+        print("Parsing", path)
+
+        with open(path, 'rb') as handle:
+            data_pickle = pickle.load(handle)
+            run_harness(data_pickle, dir_path)
+
+    index = 0
+
+    return len(files)
+
+def run_harness(data, directory):
+    print("Processing %s" % directory, data.shape)
+
+
+@click.command()
+@click.argument("input_dir", default='refine-input', type=click.Path(exists=True, file_okay=False, dir_okay=True))
+@click.argument("output_dir", default='output', type=click.Path(exists=False, file_okay=False, dir_okay=True))
+def main(input_dir, output_dir):
+
+    if not os.path.exists(input_dir):
+        raise Exception('The directory does not exist at path {}'.format(input_dir)) 
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    start = time.time()
+
+    num_files = parse_data(input_dir, output_dir)
+    elapsed = (time.time() - start)
+
+    print("Processing %d images took %.2f seconds (%.2fs per image)" % (num_files, elapsed, elapsed/num_files))
+
+if __name__ == "__main__":
+    main()
