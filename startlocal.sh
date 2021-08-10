8e8a571c27793375536bf67bcb64120980857c28
Joel Teply <joel@cambriantech.com>
2021-08-10 15:54:26 -0500

    better messaging

--- startlocal.sh
+++ startlocal.sh
@@ -1,5 +1,6 @@
 sudo sh stoplocal.sh
 
+echo "Creating local..."
 echo "Creating network"
 sudo docker network create --driver bridge cb-backend
 
@@ -10,5 +11,8 @@ echo "Starting imageproc"
 sudo docker run --name imageproc --gpus all -e PLANES_ADDRESS="http://planes:8081/" -e USER_UPLOADS_BUCKET=cb-user-image-uploads -e RESULTS_BUCKET=cb-user-image-uploads -p 8080:8080 -dt imageproc
 
 echo "Connecting networks"
+
 sudo docker network connect cb-backend planes
 sudo docker network connect cb-backend imageproc
+
+echo "Done. Creating local"
--- stoplocal.sh
+++ stoplocal.sh
@@ -1,5 +1,14 @@
-echo "Stopping existing containers"
+echo "\nStopping existing containers..."
+
+echo "Stopping planes"
 sudo docker container kill planes
 sudo docker container rm planes
+
+echo "Stopping imageproc"
 sudo docker container kill imageproc 
 sudo docker container rm imageproc
+
+echo "Removing network cb-backend"
+sudo docker network rm cb-backend
+
+echo "Done. Stopping existing containers.\n"
