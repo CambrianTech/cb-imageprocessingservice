177eae4fc81c8f18b989f9fa39e8fbd44046aaa5
Joel Teply <joel@cambriantech.com>
2021-09-01 17:59:41 -0500

    bug fix

--- pipeline/linefinder.py
+++ pipeline/linefinder.py
@@ -10,9 +10,13 @@ from pipeline.core import PipelineStep, PipelineStepIndex
 from pipeline.logging import log_image, im_logging_enabled, LogLevel
 
 class Line(Sequence):
-    def __init__(self, data):
+    def __init__(self, data, sx=1, sy=1):
         super().__init__()
         self.data = data
+        self.data[0] *= sx
+        self.data[2] *= sx
+        self.data[1] *= sy
+        self.data[3] *= sy
         self.recalculate()
 
     def __getitem__(self, i):
@@ -28,7 +32,7 @@ class Line(Sequence):
     def point_b(self):
         return (self.data[2], self.data[3])
 
-    def draw(self, img, color=(255,0,0,255), thickness=1):
+    def draw(self, img, color=(180,255,100,255), thickness=1):
         cv2.line(img, self.point_a, self.point_b, color, thickness)
 
     def reshape(self, *args):
@@ -64,7 +68,12 @@ class PipelineLineFinder(PipelineStep):
 
         fld = cv2.ximgproc.createFastLineDetector(int(self.diagonal / 60.0), 1.41, 200, 240, 3, False)
 
+        sx = data["image"].shape[1] / data["hed"].shape[1]
+        sy = data["image"].shape[0] / data["hed"].shape[0]
         lines = list(map(lambda x: Line(x.reshape(4)), fld.detect(bw)))
+
+        lines.extend(list(map(lambda x: Line(x.reshape(4), sx, sy), fld.detect(data["hed"]))))
+
 
         if im_logging_enabled(data, LogLevel.Lines):
             debug = data["image"].copy()
