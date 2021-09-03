99d5b2f01f4fb977f61939b8b2ce8af101b81931
Joel Teply <joel@cambriantech.com>
2021-09-03 13:27:53 -0500

    fixes

--- pipeline/Line.py
+++ pipeline/Line.py
@@ -1,3 +1,5 @@
+import math
+import numpy as np
 import numba as nb
 from numba.experimental import jitclass
 
@@ -39,5 +41,82 @@ class Line:
         return (self.x1, self.y1)
 
     def draw(self, img, color=(255,50,255,255), thickness=2):
-        cv2.line(img, self.point_a, self.point_b, color, thickness)    
+        cv2.line(img, self.point_a, self.point_b, color, thickness)  
+
+
+@nb.jit(nopython=True)
+def line_angle_difference(x, y): #minimum angle between lines segments cannot differ by more than 90 degrees
+    diff = abs(math.atan2(math.sin(x-y), math.cos(x-y)))
+    if diff > 0.5 * math.pi:
+        diff = math.pi - diff
+
+    return diff
+
+@nb.jit(nopython=True)
+def line_angle(x0, y0, x1, y1):
+    #return np.arctan2(y1 - y0, x1 - x0)
+    return math.atan2(float(y1 - y0), float(x1 - x0))
+
+@nb.jit(nopython=True)
+def bounding_box(line, width, length_multiplier=1.0):
+    return (line.midpoint, (line.length * length_multiplier, width), np.degrees(line.angle))
+
+@nb.jit(nopython=True)
+def merge_line_pair(line_a, line_b):
+
+    ax = line_a[0][0]
+    ay = line_a[0][1]
+    bx = line_a[1][0]
+    by = line_a[1][1]
+
+    cx = line_b[0][0]
+    cy = line_b[0][1]
+    dx = line_b[1][0]
+    dy = line_b[1][1]
+    
+    thi = 0.0; 
+    thj = 0.0; 
+    thr = 0.0;
+
+    dlix = (bx - ax);
+    dliy = (by - ay);
+    dljx = (dx - cx);
+    dljy = (dy - cy);
+
+    li = math.sqrt((dlix * dlix) + (dliy * dliy));
+    lj = math.sqrt((dljx * dljx) + (dljy * dljy));
+
+    xg = (li * (ax + bx) + lj * (cx + dx)) / (2.0 * (li + lj));
+    yg = (li * (ay + by) + lj * (cy + dy)) / (2.0 * (li + lj));
+
+    if (dlix == 0.0): thi = math.pi / 2.0;
+    else: thi = math.atan(dliy / dlix);
+
+    if (dljx == 0.0): thj = math.pi / 2.0;
+    else: thj = math.atan(dljy / dljx);
+
+    if abs(thi - thj) <= math.pi / 2.0:
+        thr = (li * thi + lj * thj) / (li + lj);
+    else:
+        tmp = thj - math.pi * (thj / abs(thj));
+        thr = li * thi + lj * tmp;
+        thr /= (li + lj);
+
+    sin_thr = math.sin(thr)
+    cos_thr = math.cos(thr)
+
+    axg = (ay - yg) * sin_thr + (ax - xg) * cos_thr;
+    bxg = (by - yg) * sin_thr + (bx - xg) * cos_thr;
+    cxg = (cy - yg) * sin_thr + (cx - xg) * cos_thr;
+    dxg = (dy - yg) * sin_thr + (dx - xg) * cos_thr;
+
+    delta1xg = min(axg,min(bxg,min(cxg,dxg)));
+    delta2xg = max(axg,max(bxg,max(cxg,dxg)));
+
+    delta1x = delta1xg * cos_thr + xg;
+    delta1y = delta1xg * sin_thr + yg;
+    delta2x = delta2xg * cos_thr + xg;
+    delta2y = delta2xg * sin_thr + yg;
+
+    return (delta1x, delta1y), (delta2x, delta2y)  
 
--- pipeline/linefinder.py
+++ pipeline/linefinder.py
@@ -10,6 +10,7 @@ from cambrian.frei_chen import frei_chen
 
 from pipeline.core import PipelineStep, PipelineStepIndex
 from pipeline.logging import log_image, im_logging_enabled, LogLevel
+from pipeline.Line import Line, line_angle_difference, bounding_box
 
 def point_segment_distance(px, py, x1, y1, x2, y2):
   dx = x2 - x1
@@ -46,52 +47,6 @@ def sharpen(img, alpha=1.5, beta=-1.0, kernel_size = 21):
     smoothed = cv2.GaussianBlur(img, (kernel_size, kernel_size), kernel_size)
     return cv2.addWeighted(img, alpha, smoothed, beta, 0)
 
-class Line(Sequence):
-    def __init__(self, data, sx=1, sy=1):
-        super().__init__()
-        self.data = data
-        self.data[0] *= sx
-        self.data[2] *= sx
-        self.data[1] *= sy
-        self.data[3] *= sy
-        self.recalculate()
-
-        self.dead = False
-
-    def __getitem__(self, i):
-        return self.data[i]
-
-    def __len__(self):
-        return len(self.data)
-
-    def __lt__(self, other):
-        return self.angle < other.angle
-    
-    def __eq__(self, other):
-        return self.angle == other.angle
-
-    @property
-    def point_a(self):
-        return (self.data[0], self.data[1])
-
-    @property
-    def point_b(self):
-        return (self.data[2], self.data[3])
-
-    def draw(self, img, color=(255,50,255,255), thickness=2):
-        cv2.line(img, self.point_a, self.point_b, color, thickness)
-
-    def reshape(self, *args):
-        return self.data.reshape(*args)
-
-    def recalculate(self):
-        self.midpoint = ((self.point_a[0] + self.point_b[0]) / 2, (self.point_a[1] + self.point_b[1]) / 2)
-        self.length = distance.euclidean(self.point_a, self.point_b)
-        self.angle = LineFunctions.line_angle(self.point_a[0], self.point_a[1], self.point_b[0], self.point_b[1])
-
-    def bounding_box(self, width, length_multiplier=1.0):
-        return (self.midpoint, (self.length * length_multiplier, width), np.degrees(self.angle))
-
 class PipelineLineFinder(PipelineStep):
 
     @property
@@ -110,30 +65,27 @@ class PipelineLineFinder(PipelineStep):
     #todo: write in C or lambda
     def merge(self, lines, search_width, search_length=1.01, angle_threshold=math.radians(3)):
 
-        lines = sorted(lines)
         min_dist_sq = search_width * search_width
 
-        i=0
-        while i < len(lines):
+        for i in range(len(lines)):
             line_a = lines[i]
-            i += 1
-
+            
             if line_a.dead: continue
 
-            rect_a = line_a.bounding_box(search_width, length_multiplier=search_length)
+            rect_a = bounding_box(line_a, search_width, length_multiplier=search_length)
             data = (line_a.point_a, line_a.point_b)
 
-            for line_b in lines:
-
+            for j in range(len(lines)):
+                line_b = lines[j]
                 #Optimization possible: line_angle_difference should not be required by bisect methods above returning only angles in range
-                if line_a == line_b or line_b.dead or LineFunctions.line_angle_difference(line_a.angle, line_b.angle) > angle_threshold: continue
+                if i == j or line_b.dead or line_angle_difference(line_a.angle, line_b.angle) > angle_threshold: continue
 
                 dist_sq = distance.sqeuclidean(line_a.midpoint, line_b.midpoint)
 
                 if dist_sq <= min_dist_sq:
                     result = 1
                 else:
-                    rect_b = line_b.bounding_box(search_width, length_multiplier=search_length)
+                    rect_b = bounding_box(line_b, search_width, length_multiplier=search_length)
                     result, _ = cv2.rotatedRectangleIntersection(rect_a, rect_b)
 
                 if result != 0:
@@ -143,7 +95,7 @@ class PipelineLineFinder(PipelineStep):
 
 
             if line_a.dead:
-                new_line = Line(np.array([(data[0][0], data[0][1], data[1][0], data[1][1])], dtype=np.int).reshape(4))
+                new_line = Line(data[0][0], data[0][1], data[1][0], data[1][1])
                 lines.insert(i, new_line)
 
         return list(filter(lambda x: not x.dead, lines))
@@ -164,7 +116,7 @@ class PipelineLineFinder(PipelineStep):
                 log_image(data, name, debug)
 
         #transform_result = lambda x: list(map(lambda x: Line(x.reshape(4), sx, sy), result))
-        transform_lines = lambda result: list(map(lambda x: Line(x[0][0] * sx, x[0][1] * sy, x[0][2] * sx, x[0][3] * sy), result))
+        transform_result = lambda result: list(map(lambda x: Line(x[0][0] * sx, x[0][1] * sy, x[0][2] * sx, x[0][3] * sy), result))
 
         fld = cv2.ximgproc.createFastLineDetector(int(diagonal / 60.0), 1.41, 200, 240, 3, False)
         lines = []
