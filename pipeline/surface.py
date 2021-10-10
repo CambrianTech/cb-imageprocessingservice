896da77333650ad9f97fa19db759221876c435a9
Joel Teply <joel@cambriantech.com>
2021-10-09 19:18:22 -0500

    use contour area

--- pipeline/surface.py
+++ pipeline/surface.py
@@ -72,12 +72,12 @@ class Surface():
     @property
     def lines(self) -> list:
         if self._lines is None:
-            padding = self.geometry.image.shape[1] / 30
-
             self._lines = []
             for i in range(len(self.data["lines"])):
                 line = self.data["lines"][i]
                 for contour in self.contours:
+                    area = cv2.contourArea(contour)
+                    padding = math.sqrt(area) / 10
                     dist_midpoint = cv2.pointPolygonTest(contour, line.midpoint, True)
                     dist_a = cv2.pointPolygonTest(contour, line.point_a, True)
                     dist_b = cv2.pointPolygonTest(contour, line.point_b, True)
