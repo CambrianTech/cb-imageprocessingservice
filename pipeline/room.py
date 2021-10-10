9d1f724a1e34632b072052caa579dfe172b0f51d
Joel Teply <joel@cambriantech.com>
2021-10-09 21:41:48 -0500

    comment

--- pipeline/room.py
+++ pipeline/room.py
@@ -46,7 +46,7 @@ class Room(Geometry):
         self.analyze_surfaces()
         log_image(self.data, "room_initial", self.get_debug_image())
 
-        self.refine_surfaces(min_confidence=0.1) #preserve plane context information i.e. probs < 0.1 are ignored
+        self.refine_surfaces(min_confidence=0.1) #preserve plane context information i.e. probs < min_confidence are ignored
         log_image(self.data, "room_refined", self.get_debug_image())
 
         self.add_missing_surfaces()
