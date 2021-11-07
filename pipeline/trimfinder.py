a795ddb99de5c09218bb42c86b91f542c04d2c43
Joel Teply <joel@cambriantech.com>
2021-11-07 09:09:04 -0600

    ransac

--- pipeline/trimfinder.py
+++ pipeline/trimfinder.py
@@ -4,31 +4,63 @@ import cv2
 import random
 import time
 import math
+from operator import attrgetter
 
 from .core import PipelineStep, PipelineStepIndex, SurfaceType
 from .logging import log_image, im_logging_enabled
 from .utils import convert_color
 from .Line import Line
 
+class TrimLine:
+    def __init__(self, vp, primary_line, lines):
+        self.vp = vp
+        self.primary_line = primary_line
+        self.lines = lines
+
+    @property
+    def inlier_lines(self):
+        if self._inlier_lines is None:
+            self._inlier_lines = list(map(lambda line_data: Line(line_data), self.inliers))
+
+        return self._inlier_lines
+
 class TrimFinder():
-    def __init__(self, data, surface):
+    def __init__(self, data, surface, vp):
         super().__init__()
         self.data = data
         self.room = data["room"]
         self.image = self.data["downscaled"]
         self.surface = surface
+        self.vp = vp
 
-    def solve(self):
+    def solve(self, max_iterations=500, max_time=0.25):
         
-        if self.surface.horizontal_vp is None or len(self.surface.horizontal_vp) == 0:
+        seeds = sorted(self.vp.inlier_lines, key=attrgetter('length'), reverse=True)
+        lines = sorted(self.surface.lines, key=attrgetter('length'), reverse=True)
+
+        if len(seeds) == 0:
+            seeds = lines
+
+        num_seeds = len(seeds)
+        num_lines = len(lines)
+
+        if num_seeds < 1 or num_lines < 1:
             return []
 
-        surface_barriers = self.surface.horizontal_vp[0].inlier_lines
+        start_time = time.time()
+
+        for ransac_iter in range(max_iterations):
+            if time.time() - start_time > max_time:
+                break
+
+            ind1 = random.randint(0, num_seeds-1)
+            ind2 = random.randint(0, num_lines-1)
+
 
         # for line in self.surface.horizontal_vp[0].inlier_lines:
         #     surface_barriers.extend(line)
             
-        return surface_barriers
+        return seeds
         
             
 class PipelineTrimFinder(PipelineStep):
@@ -55,8 +87,15 @@ class PipelineTrimFinder(PipelineStep):
         self.surfaces = self.room.get_surfaces(surfaceTypes=[SurfaceType.Wall, SurfaceType.WallLike, SurfaceType.Floor, SurfaceType.Ceiling])
 
         for surface in self.surfaces:
-            tf = TrimFinder(self.data, surface)
-            surface.trim_lines = tf.solve()
+            surface.trim_lines = []
+
+            if surface.horizontal_vp is not None and len(surface.horizontal_vp) > 0:
+                tf = TrimFinder(self.data, surface, surface.horizontal_vp[0])
+                surface.trim_lines.extend(tf.solve())
+
+            if surface.vertical_vp is not None and len(surface.vertical_vp) > 0:
+                tf = TrimFinder(self.data, surface, surface.vertical_vp[0])
+                surface.trim_lines.extend(tf.solve())
 
         if im_logging_enabled(self.data):
             log_image(self.data, "trim.png", self.get_debug_image())
@@ -74,7 +113,8 @@ class PipelineTrimFinder(PipelineStep):
             surface = self.surfaces[i]
             color = convert_color((hues[i],127,255), cv2.COLOR_HSV2RGB_FULL)
 
-            Line.draw_all(img, surface.trim_lines, color=color, thickness=2)
+            if surface.trim_lines is not None:
+                Line.draw_all(img, surface.trim_lines, color=color, thickness=2)
 
 
         return img
