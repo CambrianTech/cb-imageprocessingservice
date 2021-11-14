171668b78b6c677a227ba62601cca33268b16919
Joel Teply <joel@cambriantech.com>
2021-11-14 13:26:20 -0600

    bug

--- pipeline/Line.py
+++ pipeline/Line.py
@@ -42,10 +42,7 @@ class Line(Sequence):
         return len(self.data)
 
     def __lt__(self, other):
-        return self.angle < other.angle
-    
-    def __eq__(self, other):
-        return self.angle == other.angle
+        return line_angle_difference(self.angle, other.angle)
 
     @property
     def point_a(self):
@@ -81,30 +78,28 @@ class Line(Sequence):
     #     return rotated_rects_points(self.midpoint, (self.length * length_multiplier, width), self.angle)
 
     def in_range(self, lines, angle_threshold):
-        return list(filter(lambda line: not line.dead 
-                                        and LineFunctions.line_angle_difference(self.angle, line.angle) < angle_threshold 
-                                        and line != self, lines)) 
+        return list(filter(lambda line: not line.dead and LineFunctions.line_angle_difference(self.angle, line.angle) <= angle_threshold, lines)) 
 
     @classmethod
     def draw_all(cls, img, lines, color=(255,50,255,255), thickness=1, sx=1.0, sy=1.0, lineType=cv2.LINE_8):
         [line.draw(img, color=color, thickness=thickness, sx=sx, sy=sy, lineType=lineType) for line in lines]
 
     @classmethod
-    def merge(cls, lines, search_width, search_length=1.01, angle_threshold=math.radians(3), max_iterations=1e5):
+    def merge(cls, lines, search_width, search_length=1.01, angle_threshold=math.radians(3)):
     
         min_dist_sq = search_width * search_width
 
-        iterations = 0
+        lines.sort()
+
         for i in range(len(lines)):
-            if (iterations > max_iterations): break
+            
             line_a = lines[i]
-            iterations += 1
             if line_a.dead: continue
 
             rect_a = line_a.bounding_box(search_width, length_multiplier=search_length)
             data = (line_a.point_a, line_a.point_b)
 
-            candidates = line_a.in_range(lines, angle_threshold)
+            candidates = line_a.in_range(lines[:i] + lines[i+1:], angle_threshold)
 
             for line_b in candidates:
 
@@ -123,7 +118,7 @@ class Line(Sequence):
 
 
             if line_a.dead:
-                lines[i] = Line(np.array([data[0][0], data[0][1], data[1][0], data[1][1]], dtype=np.int), group=lines[i].group, id=lines[i].id)
+                lines[i] = Line(np.array([data[0][0], data[0][1], data[1][0], data[1][1]], dtype=np.int), group=line_a.group, id=line_a.id)
 
         return list(filter(lambda x: not x.dead, lines))
 
