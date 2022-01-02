bd7d4d2f8c61d66b703e3159071ed11b4ff58eba
Joel Teply <joel@cambriantech.com>
2022-01-01 22:48:24 -0600

    temporary min size for some reason needed on front end

--- pipeline/stages/output.py
+++ pipeline/stages/output.py
@@ -112,12 +112,16 @@ class PipelineOutput(PipelineStep):
                 planes_alpha_mask_url = self.make_url(filename)
                 self.save_image(data["planes_alpha_mask"], filename, planes_alpha_mask_url)
 
-                #tempoary:
+                #temporary:
                 surfaces = data["room"].surfaces
                 for i in range(len(surfaces)):
                     surface = surfaces[i]
                     mask_url = self.make_plane_mask_url(i)
-                    self.save_image(surface.mask * 255, filename, mask_url)
+                    mask = surface.mask * 255
+                    scale = 1500 / min(mask.shape[0], mask.shape[1])
+                    if scale > 1:
+                        mask = cv2.resize(mask, (int(scale * surface.mask.shape[1]), int(scale * surface.mask.shape[0])))
+                    self.save_image(mask, filename, mask_url)
 
                 results = self.make_data_v4_dict(data, image_url, lighting_url, planes_index_mask_url, planes_alpha_mask_url)
 
