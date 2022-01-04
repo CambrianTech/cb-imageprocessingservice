5d78418677f270a7e099a1297bc8302940b07eb6
Joel Teply <joel@cambriantech.com>
2022-01-03 19:39:28 -0600

    decent

--- pipeline/stages/surfacerefinement.py
+++ pipeline/stages/surfacerefinement.py
@@ -24,7 +24,7 @@ class SurfaceRefinement():
         self.image = self.data["image"]
         self.barriers = None #self.data["barriers"]
         
-    def refine(self, use_HED=True):
+    def refine(self, use_HED=False):
         self.data["lighting"] = cv2.edgePreservingFilter(np.uint8(self.data["lighting"]), flags=1, sigma_s=10, sigma_r=1.0)
         log_image(self.data, 'lighting_smooth', self.data["lighting"])
 
@@ -32,69 +32,83 @@ class SurfaceRefinement():
         total_mask = np.sum(np.dstack(masks), axis=-1, dtype=np.uint8)
         disputed_areas = np.zeros(total_mask.shape, dtype=np.uint8)
         disputed_areas[total_mask > 1] = 1
-
-        if use_HED:
-            watershed_image = cv2.resize(self.data["hed"], (self.image.shape[1], self.image.shape[0]))
-        else:
-            denoised = rank.median(self.image[:,:,1], disk(2))
-            watershed_image = rank.gradient(denoised, disk(2))
-
-        watershed_mask = np.ones(watershed_image.shape, dtype=np.int32)
-        markers = np.zeros(watershed_image.shape, dtype=np.int32)
-
-        def draw_surface_markers(surface, mask, color, freedom=0.15):
-            if surface.surfaceType == SurfaceType.OnFloor:
-                watershed_mask[mask > 0] = 0
-            dist_transform = cv2.distanceTransform(mask, distanceType=cv2.DIST_L2, maskSize=3, dstType=cv2.CV_8U)
-            markers[dist_transform > freedom * dist_transform.max()] = color
-
-        sy = self.data["image"].shape[0] / self.data["downscaled"].shape[0]
-        sx = self.data["image"].shape[1] / self.data["downscaled"].shape[1]
-
-        def draw_barrier_markers(barrier_groups):
-            for barrier in barrier_groups:
-                barrier.line.draw(markers, color= -1, thickness=1, lineType=cv2.LINE_4, sx=sx, sy=sy)
-                barrier.line.draw(watershed_mask, color=0, thickness=1, lineType=cv2.LINE_4, sx=sx, sy=sy)
-
         num_surfaces = len(self.room.surfaces)
+
+        def run_watershed(src, freedom=0.15):
+
+            if len(src.shape) == 3:
+                src = src[:,:,1]
+
+            watershed_mask = np.ones(src.shape, dtype=np.int32)
+            markers = np.zeros(src.shape, dtype=np.int32)
+
+            def draw_surface_markers(surface, mask, color):
+                if surface.surfaceType == SurfaceType.OnFloor:
+                    watershed_mask[mask > 0] = 0
+                dist_transform = cv2.distanceTransform(mask, distanceType=cv2.DIST_L2, maskSize=3, dstType=cv2.CV_8U)
+                markers[dist_transform > freedom * dist_transform.max()] = color
+
+            sy = self.data["image"].shape[0] / self.data["downscaled"].shape[0]
+            sx = self.data["image"].shape[1] / self.data["downscaled"].shape[1]
+
+            def draw_barrier_markers(barrier_groups):
+                for barrier in barrier_groups:
+                    barrier.line.draw(markers, color= -1, thickness=1, lineType=cv2.LINE_4, sx=sx, sy=sy)
+                    barrier.line.draw(watershed_mask, color=0, thickness=1, lineType=cv2.LINE_4, sx=sx, sy=sy)
+
+            for index in range(num_surfaces):
+                surface = self.room.surfaces[index]
+                draw_surface_markers(surface, mask=masks[index], color=index+1)
+
+            if self.barriers is not None:
+                lines_mask = None
+                for sb in self.barriers.values():
+                    draw_barrier_markers(sb.barrier_groups)
+            else:
+                lines_mask = np.zeros(self.image.shape[:2], dtype=np.uint8)
+                Line.draw_all(src, self.data["lines"], color=(255,255,255), thickness=1, lineType=cv2.LINE_4, sx=sx, sy=sy)
+                #watershed_mask[lines_mask > 0] = 0
+
+            markers[disputed_areas > 0] = 0
+
+            log_markers(self.data, "room_markers", markers)
+
+            markers = np.int32(watershed(src, markers, mask=watershed_mask))
+            markers[markers<0] = 0
+
+            log_markers(self.data, "room_markers_result", markers)
+
+            #set masks:
+            for index in range(num_surfaces):
+                surface = self.room.surfaces[index]
+
+                if surface.surfaceType == SurfaceType.OnFloor: 
+                    continue
+
+                color = index + 1
+                mask = np.zeros_like(total_mask)
+                mask[markers == color] = 1
+                if lines_mask is not None:
+                    mask[lines_mask > 0] = 0
+
+                kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
+                mask = cv2.dilate(mask, kernel)
+                #mask[watershed_mask == 0] = 0
+                masks[index] = mask
+
+
+        hed = cv2.resize(self.data["hed"], (self.image.shape[1], self.image.shape[0]))
+        run_watershed(hed)
+
+        #denoised = rank.median(self.image[:,:,1], disk(5))
+        denoised = cv2.bilateralFilter(self.image[:,:,1],9,20,20)
+        watershed_image = rank.gradient(denoised, disk(5))
+        run_watershed(watershed_image, freedom=0.03)
+
+        #commit changes
         for index in range(num_surfaces):
             surface = self.room.surfaces[index]
-            draw_surface_markers(surface, mask=masks[index], color=index+1)
-
-        if self.barriers is not None:
-            lines_mask = None
-            for sb in self.barriers.values():
-                draw_barrier_markers(sb.barrier_groups)
-        else:
-            lines_mask = np.zeros(self.image.shape[:2], dtype=np.uint8)
-            Line.draw_all(watershed_image, self.data["lines"], color=(255,255,255), thickness=1, lineType=cv2.LINE_4, sx=sx, sy=sy)
-            #watershed_mask[lines_mask > 0] = 0
-            
-        markers[disputed_areas > 0] = 0
-
-        log_markers(self.data, "room_markers", markers)
-
-        markers = np.int32(watershed(watershed_image, markers, mask=watershed_mask))
-        markers[markers<0] = 0
-
-        log_markers(self.data, "room_markers_result", markers)
-
-        #set masks:
-        for index in range(num_surfaces):
-            surface = self.room.surfaces[index]
-
-            if surface.surfaceType == SurfaceType.OnFloor: 
-                continue
-
-            color = index + 1
-            mask = np.zeros_like(total_mask)
-            mask[markers == color] = 1
-            if lines_mask is not None:
-                mask[lines_mask > 0] = 0
-            kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
-            mask = cv2.dilate(mask, kernel)
-            #mask[watershed_mask == 0] = 0
-            surface.final_mask = mask
+            surface.final_mask = masks[index]
 
         log_image(self.data, "room_final", self.room.get_debug_image())
         
