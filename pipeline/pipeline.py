953c58f001fc9bb314fce0ec495bb0118d5115a3
Joel Teply <joel@cambriantech.com>
2022-02-04 19:40:39 -0600

    prepare for queue based system

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -121,33 +121,19 @@ class Pipeline():
         all_steps[PipelineStepIndex.ExtractSurfaces] = PipelineExtractSurfaces
         all_steps[PipelineStepIndex.FindLines] = PipelineLineFinder
         all_steps[PipelineStepIndex.SolveRoom] = PipelineRoomSolver
-        all_steps[PipelineStepIndex.VanishingPoints] = PipelineVanishingPointFinder
-        all_steps[PipelineStepIndex.Barriers] = PipelineBarrierFinder
-        all_steps[PipelineStepIndex.FindTrim] = PipelineTrimFinder
-        all_steps[PipelineStepIndex.FindLegs] = PipelineLegFinder
+        all_steps[PipelineStepIndex.VanishingPoints] = PipelineVanishingPointFinder if self.api_level > 3 else None
+        all_steps[PipelineStepIndex.Barriers] = PipelineBarrierFinder if self.api_level > 3 else None
+        all_steps[PipelineStepIndex.FindTrim] = PipelineTrimFinder if self.api_level > 3 else None
+        all_steps[PipelineStepIndex.FindLegs] = PipelineLegFinder if self.api_level > 3 else None
         all_steps[PipelineStepIndex.Geometry] = PipelinePlaneGeometry
         all_steps[PipelineStepIndex.EstimatePose] = PipelinePoseEstimator
         all_steps[PipelineStepIndex.Refine] = PipelineSurfaceRefinement
         all_steps[PipelineStepIndex.Superpixels] = PipelineSuperpixels if self.api_level < 3 else None
-        all_steps[PipelineStepIndex.CombinePlaneMasks] = PipelineCombinePlaneMasks
+        all_steps[PipelineStepIndex.CombinePlaneMasks] = PipelineCombinePlaneMasks if self.api_level > 3 else None
         all_steps[PipelineStepIndex.Output] = output_step
         
-        if self.api_level < 3.5:
-            all_steps[PipelineStepIndex.SolveRoom] = None
-            all_steps[PipelineStepIndex.VanishingPoints] = None
-            all_steps[PipelineStepIndex.Barriers] = None
-            all_steps[PipelineStepIndex.FindTrim] = None
-            all_steps[PipelineStepIndex.FindLegs] = None
-
-        #remove incomplete steps:
-        all_steps[PipelineStepIndex.VanishingPoints] = None
-        all_steps[PipelineStepIndex.Barriers] = None
-        all_steps[PipelineStepIndex.FindTrim] = None
-        all_steps[PipelineStepIndex.FindLegs] = None
-        all_steps[PipelineStepIndex.CombinePlaneMasks] = None
 
         #todo: deprecate these:
-
         #all_steps[PipelineStepIndex.EstimatePose] = None
         #all_steps[PipelineStepIndex.Geometry] = None
         
--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+echo USER_UPLOADS_BUCKET: $USER_UPLOADS_BUCKET
+echo RESULTS_BUCKET: $RESULTS_BUCKET
+echo PLANES_ADDRESS: $PLANES_ADDRESS
+echo SQS_QUEUE_NAME: $SQS_QUEUE_NAME
 
-#FILES_BUCKET=cb-imageprocessingservice-models
-#echo "Downloading S3 data from ${FILES_BUCKET}"
-#aws s3 cp s3://$FILES_BUCKET . --recursive
+echo "Downloading S3 data"
+aws s3 cp s3://$FILES_BUCKET . --recursive
 
 echo "Starting python serve script"
-python3 serve.py ./tensorflow_models/ ./gluon_models/ ./sklearn_models/fov_classifier_lc128.joblib hed_model/HED_pretrained_bsds.npz $USER_UPLOADS_BUCKET $RESULTS_BUCKET $PLANES_ADDRESS --api=4
+python3 serve.py ./tensorflow_models/ ./gluon_models/ ./sklearn_models/fov_classifier_lc128.joblib $USER_UPLOADS_BUCKET $RESULTS_BUCKET $PLANES_ADDRESS --sqs-queue-name $SQS_QUEUE_NAME
