ed552efe50d10397d196d1cdbc5d55816057dfaf
Joel Teply <joel@cambriantech.com>
2022-02-10 17:38:23 -0600

    export MXNET_CUDNN_AUTOTUNE_DEFAULT=0

--- start.sh
+++ start.sh
@@ -7,5 +7,6 @@ echo SQS_QUEUE_NAME: $SQS_QUEUE_NAME
 echo "Downloading S3 data"
 aws s3 cp s3://$FILES_BUCKET . --recursive
 
+export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
 echo "Starting python serve script"
 python3 serve.py ./tensorflow_models/ ./gluon_models/ ./sklearn_models/fov_classifier_lc128.joblib $USER_UPLOADS_BUCKET $RESULTS_BUCKET $PLANES_ADDRESS --sqs-queue-name $SQS_QUEUE_NAME
