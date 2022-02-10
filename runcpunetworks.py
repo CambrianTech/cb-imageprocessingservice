f1d5d99babd4b6b64020b2a0309267bd68236e30
Joel Teply <joel@cambriantech.com>
2022-02-09 22:16:00 -0600

    critical issue found

--- runcpunetworks.py
+++ runcpunetworks.py
@@ -1,17 +1,17 @@
 import os
 import pickle
+import sys
 
 from aiohttp import web
 import numpy as np
 import click
-
 from modelutils import feed_image_batched, load_model, get_session_config
-
 
 @click.command()
 @click.argument("model_path", type=click.Path(exists=True, file_okay=False, dir_okay=True))
 @click.argument("port", type=click.IntRange(0, 65535))
-def main(model_path, port):
+@click.argument("message", default="SUCCESS", type=click.STRING)
+def main(model_path, port, message):
     max_size = 100 * 1024 * 1024  # Max size to receive
 
     print("Loading models from", model_path)
@@ -19,6 +19,15 @@ def main(model_path, port):
     model_normals = load_model(os.path.join(model_path, "normals"), session_config=get_session_config(use_gpu=False))
 
     routes = web.RouteTableDef()
+
+    @routes.post("/healthcheck")
+    async def healthcheck(request):
+        print("Received healthcheck request:", request)
+        try:
+            return web.Response(body=message)
+        except Exception as e:
+            print("Error:", e)
+            return web.HTTPInternalServerError()
 
     @routes.post("/")
     async def index(request):
@@ -28,28 +37,30 @@ def main(model_path, port):
 
             # Load images as numpy array from received file.
             # Dimensions: [B, H, W, C]
+            print("Loading data from request")
             images = pickle.loads(data)
 
             lighting = feed_image_batched(model_lighting, images)
             normals = feed_image_batched(model_normals, images)
 
-            print("Processed request:", request)
+            print("Obtained lighting and normals", lighting.shape, normals.shape)
 
             return web.Response(body=pickle.dumps({
                 "lighting": lighting,
                 "normals": normals
             }))
         except Exception as e:
-           print("Error:", e)
-
+            print("Error:", e)
+        
         return web.HTTPInternalServerError()
 
     app = web.Application(client_max_size=max_size)
     app.add_routes(routes)
 
+    print(message, file=sys.stderr) #SIGNAL SUCCESS
+
     print("Starting web app on port", port)
     web.run_app(app, port=port)
-
 
 if __name__ == "__main__":
     main()
