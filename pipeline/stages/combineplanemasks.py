252e79ae76bfcf0d91af9fe38faa1e57511beba7
Joel Teply <joel@cambriantech.com>
2022-05-10 09:19:11 -0500

    is_batched true not working

--- pipeline/stages/combineplanemasks.py
+++ pipeline/stages/combineplanemasks.py
@@ -16,24 +16,28 @@ class PipelineCombinePlaneMasks(PipelineStep):
 
     @property
     def is_batched(self) -> bool:
-        return True
+        return False
+
+    def run_one(self, data):
+        room = data["room"]
+
+        index_mask = None
+
+        for i in range(len(room.surfaces)):
+            surface = room.surfaces[i]
+            mask = surface.final_mask
+
+            if index_mask is None:
+                index_mask = np.zeros_like(mask)
+
+            index_mask[mask > 0] = i + 1
+
+
+        data["index_mask"] = index_mask
 
     def run(self, data):
-        for datum in data:
 
-            room = datum["room"]
-
-            index_mask = None
-
-            for i in range(len(room.surfaces)):
-                surface = room.surfaces[i]
-                mask = surface.final_mask
-
-                if index_mask is None:
-                    index_mask = np.zeros_like(mask)
-
-                index_mask[mask > 0] = i + 1
-
-
-            datum["index_mask"] = index_mask
-
+        if self.is_batched:
+            [self.run_one(datum) for datum in data]
+        else:
+            self.run_one(data)
