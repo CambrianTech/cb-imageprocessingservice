3fbe10cf82fcd356ae2c2e865b933b5c97be77e5
Joel Teply <joel@cambriantech.com>
2022-05-23 10:45:11 -0500

    unused

--- pipeline/stages/__init__.py
+++ pipeline/stages/__init__.py
@@ -1,3 +1,3 @@
-from . import barrierfinder, combineplanemasks, edgedetector, extractsurfaces, fileinput, fileoutput, fov, \
+from . import combineplanemasks, edgedetector, extractsurfaces, fileinput, fileoutput, fov, \
     input, legfinder, lighting, linefinder, noop, output, planedetector, planegeometry, poseestimator, primaryangle, \
-    refineplanemasks, reverserenderer, scenegenerator, surfacesolver, segmentation, superpixels, surfacerefinement, trimfinder, vanishingpointfinder
+    reverserenderer, scenegenerator, surfacesolver, segmentation, superpixels, surfacerefinement, trimfinder, vanishingpointfinder
