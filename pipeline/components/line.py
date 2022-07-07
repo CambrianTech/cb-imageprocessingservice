043a6d719129402fdcdcbe959587af40dc5fc6cb
joelteply <joelteply@yahoo.com>
2022-07-07 09:43:01 -0500

    mistake

--- pipeline/components/line.py
+++ pipeline/components/line.py
@@ -361,7 +361,7 @@ def merge_lines(lines, search_width, search_length=1.05, angle_threshold=math.ra
                                                          line_b.data[0], line_b.data[1], line_b.data[2], line_b.data[3], \
                                                          line_b.dx, line_b.dy)
                 else:
-                    line_b.cluster = cluster_index
+                    line_b.cluster = line_a.cluster
 
         if line_a.dead:
             lines[i] = Line(data[0], data[1], data[2], data[3], cluster=line_a.cluster)
