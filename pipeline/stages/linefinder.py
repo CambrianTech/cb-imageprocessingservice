6a10d718747abca714aec3a1edce149ded7ef53b
joelteply <joelteply@yahoo.com>
2022-07-13 16:39:15 -0500

    remove artifacts labeling

--- pipeline/stages/linefinder.py
+++ pipeline/stages/linefinder.py
@@ -12,7 +12,8 @@ from pipeline.misc.utils import random_color
 from pipeline.data.surface_type import SurfaceType
 from pipeline.components.line import Line, merge_lines, draw_lines, line_on_image_edge
 from pipeline.core import PipelineStep, PipelineStepIndex
-from pipeline.data.logging import log_image, im_logging_enabled, LogLevel, Timer
+from pipeline.data.logging import log_image, im_logging_enabled, LogLevel
+from cambrian.LineFunctions import LineFunctions
 
 def gabor(bw, theta, lambd, gamma = 0.0, psi = 0.0):
     ksize = lambd
@@ -48,6 +49,20 @@ class PipelineLineFinder(PipelineStep):
             draw_lines(debug, lines, thickness=thickness)
             log_image(data, name, debug)
 
+        def line_cluster_image(lines):
+            cluster_image = data["downscaled"].copy()
+
+            draw_lines(cluster_image, lines, color=(0,0,0), thickness=1)
+
+            max_cluster = max(line.cluster for line in lines)
+
+            for i in range(0, max_cluster):
+                cluster_lines = list(filter(lambda x: x.cluster==i, lines))
+                if len(cluster_lines) > 0:
+                    draw_lines(cluster_image, cluster_lines, color=random_color(), thickness=2)
+
+            return cluster_image
+
         def find_lines(image, min_length, use_lsd=False, refine=cv2.LSD_REFINE_NONE, scale=1.0, sigma_scale=1.0, quant=2.0, ang_th=22.5, log_eps=0, density_th=0.7, n_bins=1024, canny_threshold=3):
             
             sx = data["downscaled"].shape[1] / image.shape[1]
@@ -63,15 +78,10 @@ class PipelineLineFinder(PipelineStep):
                 fld = cv2.ximgproc.createFastLineDetector(min_length, 1.41, 20, 240, canny_threshold, False)
                 cv_lines = fld.detect(image)
             
-            timer.log_elapsed("lines1")
             if cv_lines is not None:
                 lines.extend(map(lambda line: Line(line[0][0] * sx, line[0][1] * sy, line[0][2] * sx, line[0][3] * sy), cv_lines))
-            timer.log_elapsed("lines2")
 
             return lines
-
-        timer = Timer("line_finder")
-        timer.disable()
 
         bw = cv2.cvtColor(data["image"], cv2.COLOR_RGB2GRAY)
 
@@ -86,89 +96,60 @@ class PipelineLineFinder(PipelineStep):
         self.height, self.width = bw.shape[:2]
         diagonal = np.hypot(self.width, self.height)
 
-        timer.log_elapsed("setup")
-
         min_length = int(diagonal / 50)
         print("min_length:", min_length)
         print("diagonal:", diagonal, self.width, self.height)
-
 
         lines = list()
 
         #find lines in BW image
         bw_lines_a = find_lines(bw, min_length)
-
+        log_lines(bw_lines_a, "bw_lines_a")
         lines.extend(bw_lines_a)
 
-        timer.log_elapsed("bw_lines_a")
-
         bw_lines_b = find_lines(bw, min_length, True, ang_th=5) #ang_th=22.5 was getting false positives
+        log_lines(bw_lines_b, "bw_lines_b")
         lines.extend(bw_lines_b)
 
-        timer.log_elapsed("bw_lines_b")
-
         bw_lines_c = find_lines(bw, 2*min_length, canny_threshold=7)
+        
+        if im_logging_enabled(data):
+            merge_lines(bw_lines_c, search_width=max(diagonal/50, 3), angle_threshold=math.radians(1.0), remove_matches=False)
+            log_image(data, "bw_lines_c_clusters", line_cluster_image(bw_lines_c))
 
+        log_lines(bw_lines_c, "bw_lines_c")
         lines.extend(bw_lines_c)
 
-        log_lines(lines, "image_lines")
-
-        timer.log_elapsed("merge_lines BW")
 
         #find lines in hed hed edges
         hed_lines = find_lines(data["hed"], min_length, use_lsd=False, canny_threshold=3) #ang_th=22.5 was getting false positives
-        timer.log_elapsed("hed")
         hed_lines = merge_lines(hed_lines, search_width=diagonal/250, angle_threshold=math.radians(3))
 
         data["hed_lines"] = hed_lines
 
-
-        timer.log_elapsed("merge_lines HED")
-
-        if len(hed_lines) > 0: 
-            log_lines(hed_lines, "hed_lines")
-            # lines.extend(hed_lines)
+        log_lines(hed_lines, "hed_lines")
 
         #find lines in normals
         min_length = int(diagonal / 20)
         normals = np.uint8(data["normals"])
-        #log_image(data, "normals", normals)
         normals = cv2.split(normals)
         normals_lines = []
         for i in range(0, 3):
             normals_lines.extend(find_lines(normals[i], min_length))
 
-        timer.log_elapsed("normals_lines")
-        
-        if len(normals_lines) > 0:
-            #cleanup normals
-            # normals_lines = merge_lines(normals_lines, search_width=diagonal/300)
-            timer.log_elapsed("merge_lines normals_lines")
-
-            # lines.extend(normals_lines)
         data["normals_lines"] = normals_lines
+        log_lines(normals_lines, "normals_lines")
 
         #merge all
         lines = merge_lines(lines, search_width=max(diagonal/500, 3), search_length=1.1, angle_threshold=math.radians(5.0))
-        timer.log_elapsed("merge_lines final")
 
         #assign clusters:
         merge_lines(lines, search_width=max(diagonal/150, 3), search_length=1.2, angle_threshold=math.radians(5.0), remove_matches=False)
         log_lines(lines, "merged_lines")
 
         if im_logging_enabled(data):
-            cluster_image = data["downscaled"].copy()
-
-            draw_lines(cluster_image, lines, color=(0,0,0), thickness=1)
-
-            max_cluster = max(line.cluster for line in lines)
-
-            for i in range(0, max_cluster):
-                cluster_lines = list(filter(lambda x: x.cluster==i, lines))
-                if len(cluster_lines) > 0:
-                    draw_lines(cluster_image, cluster_lines, color=random_color(), thickness=2)
-
-            log_image(data, "cluster_image", cluster_image)
+            cluster_image = line_cluster_image(lines)
+            log_image(data, "line_clusters", cluster_image)
 
         data["lines"] = lines
 
