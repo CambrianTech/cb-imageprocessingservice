3e4f348640dcfa63e14a3ac7f29e819bfb7c5848
joelteply <joelteply@yahoo.com>
2022-07-17 10:49:21 -0500

    semantic refinement

--- pipeline/stages/extractsurfaces.py
+++ pipeline/stages/extractsurfaces.py
@@ -54,6 +54,62 @@ class PipelineExtractSurfaces(PipelineStep):
     def output_keys(self) -> list:
         return ["output", "isolated"]
 
+    def refine_semantics(self, data):
+
+        output = data["semantic_probs"]
+        labels = np.argmax(np.dstack(output), -1)
+
+        image = data["downscaled"]
+        markers = np.zeros(image.shape[:2], dtype=np.int32)
+        watershed_image = cv2.resize(data["hed"], (image.shape[1], image.shape[0]))
+        watershed_mask = np.ones(markers.shape, dtype=np.int32)
+
+        min_matches = 100
+        num_labels = np.amax(labels) + 1
+        freedom = 0.07
+
+        color = 1
+        for label in range(0, num_labels):
+
+            label_mask = labels == label
+
+            if len(labels[label_mask]) > min_matches:
+                mask = np.zeros(image.shape[:2], dtype=np.uint8)
+                mask[label_mask] = 1
+
+                #padded transform
+                mask_padded = cv2.copyMakeBorder(mask, 1, 1, 1, 1, cv2.BORDER_CONSTANT, value=0)
+                dist_transform = cv2.distanceTransform(mask_padded, cv2.DIST_L2, 5)
+                dist_transform = dist_transform[1:-1,1:-1]
+
+                value = label + 1
+                if value in [ADE20K.ceiling, ADE20K.wall]:
+                    freedom = 0.2
+                elif value in on_wall:
+                    freedom = 0.05
+                elif value in [ADE20K.floor]:
+                    freedom = 0.03
+                elif value in on_floor:
+                    freedom = 0.03
+                elif value in box_like:
+                    freedom = 0.03
+                elif value in on_ceiling:
+                    freedom = 0.05
+                else:
+                    watershed_mask[label_mask] = 0
+                    continue
+
+                markers[dist_transform > freedom * dist_transform.max()] = color
+                color += 1
+
+        log_markers(data, "label_markers", markers, primary=True, num_labels=color)
+
+        markers = np.int32(watershed(watershed_image, markers, mask=watershed_mask))
+        markers[markers<0] = 0
+
+        log_segmentation_image(data, "semantic_refinement", markers, data["downscaled"])
+
+
     def run(self, data):
 
         data["lighting"] = cv2.edgePreservingFilter(np.uint8(data["lighting"]), flags=1, sigma_s=10, sigma_r=1.0)
@@ -83,48 +139,6 @@ class PipelineExtractSurfaces(PipelineStep):
 
             log_segmentation_image(data, "everything", labels, data["downscaled"])
 
-        image = data["downscaled"]
-        markers = np.zeros(image.shape[:2], dtype=np.int32)
-        watershed_image = cv2.resize(data["hed"], (image.shape[1], image.shape[0]))
-        watershed_mask = np.ones(markers.shape, dtype=np.int32)
 
-        min_matches = 100
-        num_labels = np.amax(labels) + 1
-        freedom = 0.07
-
-        color = 1
-        for label in range(0, num_labels):
-
-            label_mask = labels == label
-
-            if len(labels[label_mask]) > min_matches:
-                mask = np.zeros(image.shape[:2], dtype=np.uint8)
-                mask[label_mask] = 1
-
-                #padded transform
-                mask_padded = cv2.copyMakeBorder(mask, 1, 1, 1, 1, cv2.BORDER_CONSTANT, value=0)
-                dist_transform = cv2.distanceTransform(mask_padded, cv2.DIST_L2, 5)
-                dist_transform = dist_transform[1:-1,1:-1]
-
-                value = label + 1
-                if value in [ADE20K.ceiling, ADE20K.wall]:
-                    freedom = 0.2
-                elif value in [ADE20K.floor]:
-                    freedom = 0.03
-                elif value in on_floor:
-                    freedom = 0.03
-                else:
-                    watershed_mask[label_mask] = 0
-                    continue
-
-                markers[dist_transform > freedom * dist_transform.max()] = color
-                color += 1
-
-        log_markers(data, "label_markers", markers, primary=True, num_labels=color)
-
-        markers = np.int32(watershed(watershed_image, markers, mask=watershed_mask))
-        markers[markers<0] = 0
-
-        debug = overlay_image(get_markers_image(data, markers, num_labels=color), image)
-        log_image(data, "label_result", debug)
+        self.refine_semantics(data)
 
