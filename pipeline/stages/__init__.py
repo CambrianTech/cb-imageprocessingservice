977da092187766cac3050d2bbb4c8761ba42d482
joelteply <joelteply@yahoo.com>
2022-08-27 12:01:29 -0500

    unused code

--- pipeline/stages/__init__.py
+++ pipeline/stages/__init__.py
@@ -1,4 +1,4 @@
 from . import barrierfinder, digestdata, edgedetector, extractsurfaces, fileinput, fileoutput, fov, \
-    input, legfinder, lighting, linefinder, noop, output, planedetector, planegeometry, poseestimator, \
+    input, legfinder, linefinder, noop, output, planedetector, planegeometry, poseestimator, \
     reverserenderer, scenegenerator, surfacesolver, segmentation, superpixels, surfacerefinement, trimfinder, vanishingpointfinder, \
     normalsestimator
